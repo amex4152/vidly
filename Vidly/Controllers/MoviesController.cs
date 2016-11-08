6933ad7498e3f2b648f8e3eefac98f2d33ba4dca
Cristian Garibay <cristian.garibay@gmail.com>
2016-11-08 11:33:11 -0600

    Fin Seccion 2

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,7 +14,33 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            return View(movie);
+            return View(movie);            
         }
+
+        
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+        /*
+        public ActionResult Edit (int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+
+        }
+        */
     }
 }
